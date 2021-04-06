cddaa4e7781632099d46bab905bfaf75306b98bd
977106024 <977106024@qq.com>
2021-04-06 17:39:28 +0800

    vue3测试

---
+++ src/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>{{v}}</div>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+    setup() {
+        const v = ref('这是一个字符串')
+        return {v}
+    },
+}
+</script>
