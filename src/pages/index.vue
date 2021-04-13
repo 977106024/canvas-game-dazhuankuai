fdbe644035214a6520fc6912090abc8bff84405d
977106024 <977106024@qq.com>
2021-04-13 15:51:49 +0800

    键盘控制挡板移动

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,13 +1,55 @@
 <template>
-    <div>{{v}}</div>
+    <div>
+        <canvas ref="game" id="canvas" width="400" height="400"></canvas>
+    </div>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { onMounted, reactive, ref } from 'vue'
 export default {
     setup() {
         const v = ref('这是一个字符串')
-        return {v}
+        const o = reactive({a:1})
+        console.log(Reflect.get(o,'a'),'0')
+
+        const game = ref(null)
+
+        onMounted(()=>{
+             const obj = {
+                x:150,
+                y:320
+            }
+            const canvas = game.value
+            const ctx = canvas.getContext('2d')
+            ctx.fillStyle = 'orange'
+            ctx.fillRect(obj.x,320,100,15)
+
+            document.addEventListener('keydown',(e)=>{
+                const key = e.key
+                 ctx.clearRect(obj.x,320,100,15)
+                if(key === 'f'){
+                    obj.x-=50
+                }else if(key === 'j'){
+                    obj.x+=50
+                }
+                ctx.fillRect(obj.x,320,100,15)
+
+              
+            })
+        })
+
+        
+
+        
+        
+
+        return {v, game}
     },
 }
 </script>
+
+<style lang="scss" scoped>
+    canvas{
+        border: 1px solid #ccc;
+    }
+</style>
