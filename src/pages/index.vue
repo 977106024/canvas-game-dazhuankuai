460a8a379457fef169859112fc7e757bd1e0b06d
977106024 <977106024@qq.com>
2021-04-13 16:51:26 +0800

    对移动挡板代码进行整理封装

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,55 +1,104 @@
 <template>
-    <div>
-        <canvas ref="game" id="canvas" width="400" height="400"></canvas>
-    </div>
+  <div>
+    <canvas ref="game" id="canvas" width="400" height="400"></canvas>
+  </div>
 </template>
 
 <script>
-import { onMounted, reactive, ref } from 'vue'
+import { onMounted, ref } from "vue";
 export default {
-    setup() {
-        const v = ref('这是一个字符串')
-        const o = reactive({a:1})
-        console.log(Reflect.get(o,'a'),'0')
+  setup() {
+    const game = ref(null);
 
-        const game = ref(null)
+    onMounted(() => {
+      _main();
+    });
 
-        onMounted(()=>{
-             const obj = {
-                x:150,
-                y:320
-            }
-            const canvas = game.value
-            const ctx = canvas.getContext('2d')
-            ctx.fillStyle = 'orange'
-            ctx.fillRect(obj.x,320,100,15)
+    const Paddle = () => {
+      const obj = {
+        x: 150,
+        y: 320,
+      };
 
-            document.addEventListener('keydown',(e)=>{
-                const key = e.key
-                 ctx.clearRect(obj.x,320,100,15)
-                if(key === 'f'){
-                    obj.x-=50
-                }else if(key === 'j'){
-                    obj.x+=50
-                }
-                ctx.fillRect(obj.x,320,100,15)
+      obj.toLeft = () => {
+        obj.x -= 5;
+      };
+      obj.toRight = () => {
+        obj.x += 5;
+      };
+      return obj;
+    };
 
-              
-            })
-        })
+    const GameCanvas = () => {
+      const canvas = game.value;
+      const ctx = canvas.getContext("2d");
+      const g = {
+        canvas,
+        ctx,
+      };
 
-        
+      setInterval(() => {
+        // 更新
+        g.update();
 
-        
-        
+        //渲染
+        g.draw();
+      }, 1000 / 30);
 
-        return {v, game}
-    },
-}
+      return g;
+    };
+
+    const _main = () => {
+      const paddle = Paddle();
+
+      const game = GameCanvas();
+
+      game.ctx.fillStyle = "orange";
+      game.ctx.fillRect(paddle.x, 320, 100, 15);
+
+      let toLeft = false;
+      let toRight = false;
+
+      //控制挡板左右
+      document.addEventListener("keydown", (e) => {
+        const key = e.key;
+        if (key === "s") {
+          toLeft = true;
+        } else if (key === "d") {
+          toRight = true;
+        }
+      });
+
+      document.addEventListener("keyup", (e) => {
+        const key = e.key;
+        if (key === "s") {
+          toLeft = false;
+        } else if (key === "d") {
+          toRight = false;
+        }
+      });
+
+      game.update = () => {
+        if (toLeft) {
+          paddle.toLeft();
+        } else if (toRight) {
+          paddle.toRight();
+        }
+      };
+
+      game.draw = () => {
+        game.ctx.clearRect(0, 0, game.canvas.width, game.canvas.height); // 清除整个画布
+        game.ctx.fillRect(paddle.x, 320, 100, 15);
+      };
+    };
+
+    return { game };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-    canvas{
-        border: 1px solid #ccc;
-    }
+canvas {
+  border: 1px solid #ccc;
+}
 </style>
